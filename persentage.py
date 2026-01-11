3a85a6eb486bbd57b7c24638fa022c8bdea05cee
siddharthasiriparapu <siddharthasiriparapu@gmail.com>
2026-01-11 09:25:56 +0100

    Add files via upload

---
+++ persentage.py
@@ -0,0 +1,10 @@
+print("persentage calculation")
+print("enter mark obtained out of hundred")
+math=int(input("math:"))
+eng=int(input("english:"))
+art=int(input("art:"))
+hindi=int(input("hindi:"))
+sum=math+eng+art+hindi
+per=(sum/400)*100
+print("total marks out of hundred=",sum)
+print("persentage=",per)
