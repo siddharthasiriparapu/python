dd1f83294fdcab00deb16068491cc782edbce525
siddharthasiriparapu <siddharthasiriparapu@gmail.com>
2026-01-11 13:49:44 +0100

    Add files via upload

---
+++ my project4.py
@@ -0,0 +1,11 @@
+import math
+number = float(input("Enter a number to find its square root: "))
+square_root = math.sqrt(number)
+print("The square root of", number, "is", square_root)
+
+
+
+
+
+
+
