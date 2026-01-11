77cdc1c50b8ef92cd9c8c1c4d80c097b09f23935
siddharthasiriparapu <siddharthasiriparapu@gmail.com>
2026-01-11 08:57:45 +0100

    Add files via upload

---
+++ size_trees.py
@@ -0,0 +1,14 @@
+print("finding average of trees size")
+tree1=95
+tree2=70
+tree3=123
+tree4=230
+tree5=100
+sum=tree1+tree2+tree3+tree4+tree5
+avg=sum/5
+print("average of all the trees = ",avg)
+
+
+
+
+
