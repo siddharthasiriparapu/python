ce4eb7bd5574318d3e8c67074d67eb4066cda071
siddharthasiriparapu <siddharthasiriparapu@gmail.com>
2026-01-25 09:04:41 +0100

    Add files via upload

---
+++ and_or.py
@@ -0,0 +1,21 @@
+a=10
+b=12
+c=0
+
+if a and b and c :
+    print("all the numbers have boolean valaeus as true")
+else:
+    print("atleast 1 number has boolean valaeu as false")
+
+
+a=10
+b=-10
+c=0
+
+if a>0  or b>0:
+    print("ehiter of the number is greater then 0")
+else:
+    print("no number is greater then 0")
+
+
+
