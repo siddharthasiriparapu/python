e45d6c9e651c20c4035e0bbae139e5cc4348117c
siddharthasiriparapu <siddharthasiriparapu@gmail.com>
2026-02-01 09:01:37 +0100

    Add files via upload

---
+++ is op.py
@@ -0,0 +1,13 @@
+print("is and is not opperater")
+x=5
+
+if (type(x)is int):
+    print ("treu")
+else :
+    print("false")
+
+if (type(x)is not int):
+    print ("treu")
+else :
+    print("false")
+    
