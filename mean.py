970daeb65470f04d4e60da481dfa27618919c17a
siddharthasiriparapu <siddharthasiriparapu@gmail.com>
2026-02-08 09:14:04 +0100

    Add files via upload

---
+++ mean.py
@@ -0,0 +1,14 @@
+print("the mean of 40 numbers is 38 one number is entered wrong = 36 and we have to corect it by 56.")
+mean1=38
+wrong_num=36
+corect_num=56
+total_num=40
+
+sum = mean1*total_num
+print("sum= ",sum)
+
+num2=sum-((wrong_num)-(corect_num)) 
+print("num2= ",num2)
+
+mean2=num2/total_num
+print("mean after corection= ",mean2)
