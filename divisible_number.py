319bfb28283025c9eaee479a5987fa1b85e31a36
siddharthasiriparapu <siddharthasiriparapu@gmail.com>
2026-02-08 09:04:27 +0100

    Add files via upload

---
+++ divisible_number.py
@@ -0,0 +1,9 @@
+print("take 2 nubers as numenator and denomenator and check n is divisible or not.")
+num=int(input("enter your numenator: "))
+den=int(input("enter your denomenator: "))
+
+if num % den ==0:
+    print("number is divisible")
+else:
+    print("number is not divisible")
+
