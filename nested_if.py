ca2edace19ac55893d7cd51f27c1bc1c1e9982e6
siddharthasiriparapu <siddharthasiriparapu@gmail.com>
2026-03-01 09:02:51 +0100

    Add files via upload

---
+++ nested_if.py
@@ -0,0 +1,12 @@
+n=int(input("enter a number: "))
+if n == 0:
+    print("number is 0")
+else:
+    if n > 0:
+        print("number is positiv")
+    else:
+        print("number is negativ")
+
+
+
+
