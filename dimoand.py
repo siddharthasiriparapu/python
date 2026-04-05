86547ee31df80e302cf02cb82561c8d0801a4f34
siddharthasiriparapu <siddharthasiriparapu@gmail.com>
2026-04-05 15:12:22 +0200

    Add files via upload

---
+++ dimoand.py
@@ -0,0 +1,26 @@
+rowsize=int(input("enter the number of rows: "))
+if rowsize%2 == 0:
+    halfdiamond = int(rowsize/2)
+else:
+    halfdiamond = int(rowsize/2)+1
+space = halfdiamond-1
+for i in range(1,halfdiamond+1):
+    for j in range(1,space+1):
+        print(end=" ")
+    space = space-1
+    num = 1
+    for j in range(2*i-1):
+        print(end = str(num))
+        num=num+1
+    print()
+
+space=1
+for i in range(1,halfdiamond):
+    for j in range(1,space+1):
+        print(end=" ")
+    space=space+1
+    num=1
+    for j in range(1,2*(halfdiamond-i)):
+        print(end=str(num))
+        num=num+1
+    print()
